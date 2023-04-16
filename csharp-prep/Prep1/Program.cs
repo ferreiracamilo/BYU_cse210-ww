048828c1b461667363050cf528021f36913521bc
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-04-16 20:32:14 -0300

    Prep1 exercise update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        String firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? "); //use write to avoid skipping line for read command
+        String lastName = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}.");
     }
 }
