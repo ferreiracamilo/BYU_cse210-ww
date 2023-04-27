fa6240bd85230e1b908cd3da9b49f9c755cc54dc
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-04-27 00:30:45 -0300

    Update entry class

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -31,18 +31,11 @@ public class Entry{
         int randNum = rd.Next(1,5);
         Dictionary<int, string> dictPrompts = new Dictionary<int, string>(){
             {1, "What would I do better today?"},
-            {2, "What's my best achivement today?"},
+            {2, "What's my best achievement today?"},
             {3, "What was the best part of my day?"},
             {4, "Did I try hard enough?"},
             {5, "How did I see the hand of the Lord in my life today?"}
         };
-        /*
-            // This could achieved as well by
-            Random rd = new Random();
-            int randNum = rd.Next(0,4);
-            List<string> listPrompts = new List<string> { "What would I do better today?", "What's my best achivement today?", "What was the best part of my day?", "Did I try hard enough?", "How did I see the hand of the Lord in my life today?"};
-            return listPrompts[randNum];
-        */
         return dictPrompts[randNum];
     }
 }
