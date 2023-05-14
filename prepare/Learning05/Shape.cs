d9ebda93b826665b4f800417cc291a9ab1287530
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-13 21:15:01 -0300

    Learning 5 first files

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,9 @@
+public abstract class Shape{
+    protected string _color;
+
+    protected abstract string GetColor();
+
+    protected abstract void SetColor(string color);
+
+    protected abstract double GetArea();
+}
