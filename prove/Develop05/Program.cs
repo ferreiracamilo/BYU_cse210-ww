4ece34d38b9b7656cfaa7f22aa54140531b7c17e
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-14 20:23:41 -0300

    update main

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -2,8 +2,76 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop05 World!");
+    static void Main(string[] args){
+        int option = 0;
+        List<int> parameters = new List<int>();
+
+
+        do{
+            option = PrintMenu();
+            if(option != 6){
+                switch(option){
+                    case 1:
+                        int goalType;
+                        Console.Clear();
+                        goalType = PrintMenuGoalCreation();
+
+                        switch(goalType){
+                            case 1:
+                                SimpleGoal simpleGoal = new SimpleGoal();
+                                simpleGoal.AskInformation();
+                                break;
+                            case 2:
+                                EternalGoal eternalGoal = new EternalGoal();
+                                eternalGoal.AskInformation();
+                                break;
+                            case 3:
+                                ChecklistGoal checklistGoal = new ChecklistGoal();
+                                checklistGoal.AskInformation();
+                                break;
+                            case 4:
+                                ReductionGoal reductionGoal = new ReductionGoal();
+                                reductionGoal.AskInformation();
+                                break;
+                        }
+
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+        }while(option != 6);
+    }
+
+    private static int PrintMenu(){
+        int option;
+        Console.WriteLine("\nMenu Options:");
+        Console.WriteLine("    1. Create New Goal");
+        Console.WriteLine("    2. List Goals");
+        Console.WriteLine("    3. Save Goals");
+        Console.WriteLine("    4. Load Goals");
+        Console.WriteLine("    5. Record Event");
+        Console.WriteLine("    6. Quit");
+        Console.Write("Select a choice from the menu: ");
+        option = int.Parse(Console.ReadLine());
+        return option;
+    }
+
+    private static int PrintMenuGoalCreation(){
+        int option;
+        Console.WriteLine("\nThe types of Goals are");
+        Console.WriteLine("    1. Simple Goal");
+        Console.WriteLine("    2. Eternal Goal");
+        Console.WriteLine("    3. Checklist Goal");
+        Console.WriteLine("    4. Reduction Goal");
+        Console.Write("Which type of goal would you like to create? ");
+        option = int.Parse(Console.ReadLine());
+        return option;
     }
 }
