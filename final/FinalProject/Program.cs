9de258f49c9db6b567e6049777f34f7c91080860
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-21 19:16:45 -0300

    Update final project to contain the proposal

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -7,3 +7,26 @@ class Program
         Console.WriteLine("Hello FinalProject World!");
     }
 }
+
+/*
+
+I choose for my final project an 'Open-ended project' that will consist on create a simple system for a veterinary clinic.
+
+Functionalities to cover
+
+Create, Edit and Update -> Pets
+Create, Edit and Update -> Owners
+Create, Edit and Update -> Vet (to track/relate to vistis/treatments)
+Create, Edit and Update -> Treatments
+        Within Treatments there will be
+                Simple Treatment
+                Cronic Treatment
+                Surgery
+Create, Edit and Update -> Diagnostic
+Create, Edit and Update -> Pet visit
+        Within Pet Vistit there will be
+                Periodic control
+                Due to some discomfort
+Create, Edit and Update -> Drugs (to relate with treatments, except surgery)
+
+*/
