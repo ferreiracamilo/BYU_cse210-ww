0a49479cd8e7baa6f31f22a2bcf3995ebb2ba900
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-21 22:52:10 -0300

    udpate comments

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -14,19 +14,19 @@ I choose for my final project an 'Open-ended project' that will consist on creat
 
 Functionalities to cover
 
-Create, Edit and Update -> Pets
-Create, Edit and Update -> Owners
-Create, Edit and Update -> Vet (to track/relate to vistis/treatments)
+X Create, Edit and Update -> Pets
+X Create, Edit and Update -> Owners
+X Create, Edit and Update -> Vet (to track/relate to vistis/treatments)
 Create, Edit and Update -> Treatments
         Within Treatments there will be
                 Simple Treatment
                 Cronic Treatment
                 Surgery
-Create, Edit and Update -> Diagnostic
-Create, Edit and Update -> Pet visit
+X Create, Edit and Update -> Diagnostic
+X Create, Edit and Update -> Pet visit
         Within Pet Vistit there will be
                 Periodic control
                 Due to some discomfort
-Create, Edit and Update -> Drugs (to relate with treatments, except surgery)
+X Create, Edit and Update -> Drugs (to relate with treatments, except surgery)
 
 */
