89a6785f1b830f7e710f28413321e7573ac3d133
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-27 22:34:54 -0300

    Update Vet

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,17 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        int docId = 44466454;
-        string phoneNumber = "44-44";
-        string address = "444 Jim St";
-        string email = "camilo@mail.com";
+        // Console.WriteLine("The result of email validation is " + Utils.ValidateEmail(email));
 
-        Console.WriteLine("The result of email validation is " + Utils.ValidateEmail(email));
+        
 
-        Vet vet1 = new Vet(docId, phoneNumber, address, email);
+        
 
-        Console.WriteLine();
-        Console.WriteLine("Check below string representation return");
-        Console.WriteLine(vet1.StringRepresentation());
     }
 }
--- final/FinalProject/Vet.cs
+++ final/FinalProject/Vet.cs
@@ -8,6 +8,14 @@ public class Vet : IDataSL{
     private DateOnly _startDate;
     private bool _isActive;
 
+
+    /// <summary>
+    /// Constructor meant to initiliaze a new object that will set by default start date as today and status as active
+    /// </summary>
+    /// <param name="docId">int</param>
+    /// <param name="phoneNumber">string</param>
+    /// <param name="address">string</param>
+    /// <param name="email">string</param>
     public Vet(int docId, string phoneNumber, string address, string email){
         if(!Utils.ValidateEmail(email)){
             throw new EmailValidationException("Email argument is not an actual email address.");
@@ -21,6 +29,47 @@ public class Vet : IDataSL{
         _email = email;
         _startDate = DateOnly.FromDateTime(DateTime.Now);
         _isActive = true;
+    }
+
+    /// <summary>
+    /// Constructor meant to initiliaze a new object that will set by default status as active
+    /// </summary>
+    /// <param name="docId">int</param>
+    /// <param name="phoneNumber">string</param>
+    /// <param name="address">string</param>
+    /// <param name="email">string</param>
+    /// <param name="date">DateOnly</param>
+    public Vet(int docId, string phoneNumber, string address, string email, DateOnly date){
+        if(!Utils.ValidateEmail(email)){
+            throw new EmailValidationException("Email argument is not an actual email address.");
+        }
+        if(!Utils.ValidateEmail(phoneNumber)){
+            throw new PhoneNumberValidationException("Phone argument is not an actual phone number.");
+        }
+        _docId = docId;
+        _phoneNumber = phoneNumber;
+        _address = address;
+        _email = email;
+        _startDate = date;
+        _isActive = true;
+    }
+
+    /// <summary>
+    /// Constructor meant to create objects for later load functionality
+    /// </summary>
+    /// <param name="docId">int</param>
+    /// <param name="phoneNumber">string</param>
+    /// <param name="address">string</param>
+    /// <param name="email">string</param>
+    /// <param name="date">DateOnly</param>
+    /// <param name="status">bool</param>
+    public Vet(int docId, string phoneNumber, string address, string email, DateOnly date, bool status){
+        _docId = docId;
+        _phoneNumber = phoneNumber;
+        _address = address;
+        _email = email;
+        _startDate = date;
+        _isActive = status;
     }
 
     public string StringRepresentation(){
