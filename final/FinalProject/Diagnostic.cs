8cfa4878a8a1bf88bead86f7b2872852993c106b
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-28 21:59:38 -0300

    getters and setters

--- final/FinalProject/Diagnostic.cs
+++ final/FinalProject/Diagnostic.cs
@@ -6,6 +6,54 @@ public class Diagnostic : IDataSL{
     private Vet _vet;
     private int _id;
 
+    public void SetReports(List<string> reports){
+        _reports = reports;
+    }
+
+    public List<string> GetReports(){
+        return _reports;
+    }
+
+    public void SetPatient(Pet patient){
+        _patient = patient;
+    }
+
+    public Pet GetPatient(){
+        return _patient;
+    }
+
+    public void SetDate(DateOnly date){
+        _date = date;
+    }
+
+    public DateOnly GetDate(){
+        return _date;
+    }
+
+    public void SetSeverity(int severity){
+        _severity = severity;
+    }
+
+    public int GetSeverity(){
+        return _severity;
+    }
+
+    public void SetVet(Vet vet){
+        _vet = vet;
+    }
+
+    public Vet GetVet(){
+        return _vet;
+    }
+
+    public void SetId(int id){
+        _id = id;
+    }
+
+    public int GetId(){
+        return _id;
+    }
+
     public void AddReport(string report){
         _reports.Add(report);
     }
