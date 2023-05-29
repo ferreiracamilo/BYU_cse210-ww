a6b5ac18dd632a6b561a095570e07d696f4a94cb
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-28 22:43:30 -0300

    update setters and getters

--- final/FinalProject/Surgery.cs
+++ final/FinalProject/Surgery.cs
@@ -2,6 +2,22 @@ public class Surgery : Treatment, IDataSL{
     private List<string> _procedures;
     private string _anestesiaType;
 
+    public void SetProcedures(List<string> procedures){
+        _procedures = procedures;
+    }
+
+    public List<string> GetProcedures(){
+        return _procedures;
+    }
+
+    public void SetAnestesia(string anestesia){
+        _anestesiaType = anestesia;
+    }
+
+    public string GetAnestesia(){
+        return _anestesiaType;
+    }
+
     public void AddProcedure(string procedure){
         _procedures.Add(procedure);
     }
