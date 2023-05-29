064e492b3e75c8a6e6d8ac7bc47d37a8c50a069e
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-28 23:31:27 -0300

    update

--- final/FinalProject/CronicTreatment.cs
+++ final/FinalProject/CronicTreatment.cs
@@ -31,6 +31,28 @@ public class CronicTreatment : Treatment, IDataSL{
         return _drugs;
     }
 
+    /// <summary>
+    /// Turn a List<Drug> into one string concatenate with ','
+    /// </summary>
+    /// <param name="list">List<Drug></param>
+    /// <returns>string</returns>
+    private static string DrugListToString(List<Drug> list){
+        string finalword = "";
+        if(list.Count()>0){
+            for(int i=0; i<list.Count(); i++){
+                if(i != list.Count()-1){
+                    finalword += list[i].GetId() + ",";
+                }else{
+                    finalword += list[i].GetId();
+                }
+            }
+        }else{
+            finalword = " ";
+        }
+        
+        return finalword;
+    }
+
     public string StringRepresentation(){
         return "a";
     }
--- final/FinalProject/SimpleTreatment.cs
+++ final/FinalProject/SimpleTreatment.cs
@@ -13,6 +13,28 @@ public class SimpleTreatment : Treatment, IDataSL{
         _drugs.Add(drug);
     }
 
+    /// <summary>
+    /// Turn a List<Drug> into one string concatenate with ','
+    /// </summary>
+    /// <param name="list">List<Drug></param>
+    /// <returns>string</returns>
+    private static string DrugListToString(List<Drug> list){
+        string finalword = "";
+        if(list.Count()>0){
+            for(int i=0; i<list.Count(); i++){
+                if(i != list.Count()-1){
+                    finalword += list[i].GetId() + ",";
+                }else{
+                    finalword += list[i].GetId();
+                }
+            }
+        }else{
+            finalword = " ";
+        }
+        
+        return finalword;
+    }
+
     public string StringRepresentation(){
         return "a";
     }
