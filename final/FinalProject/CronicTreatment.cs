575ee86f675f9ad0a28b24614788b401c9c6bd66
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-28 21:48:32 -0300

    getter and setters

--- final/FinalProject/CronicTreatment.cs
+++ final/FinalProject/CronicTreatment.cs
@@ -7,6 +7,30 @@ public class CronicTreatment : Treatment, IDataSL{
         _drugs.Add(drug);
     }
 
+    public void SetQtyWeeksPrescripted(int weeks){
+        _qtyWeeksPrescripted = weeks;
+    }
+
+    public int GetQtyWeeksPrescripted(){
+        return _qtyWeeksPrescripted;
+    }
+
+    public void SetQtyWeeksCompleted(int weeks){
+        _qtyWeeksCompleted = weeks;
+    }
+
+    public int GetQtyWeeksCompleted(){
+        return _qtyWeeksCompleted;
+    }
+
+    public void SetDrugs(List<Drug> drugs){
+        _drugs = drugs;
+    }
+
+    public List<Drug> GetDrugs(){
+        return _drugs;
+    }
+
     public string StringRepresentation(){
         return "a";
     }
