51f0d876d8ab38dd41da06d10600ce7b26cac785
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-28 22:13:40 -0300

    getters and setters

--- final/FinalProject/Drug.cs
+++ final/FinalProject/Drug.cs
@@ -4,6 +4,40 @@ public class Drug : IDataSL{
     private string _presentation;
     private int _id;
 
+    public void SetName(string name){
+        _name = name;
+    }
+
+    public string GetName(){
+        return _name;
+    }
+
+    public void SetType(string type){
+        _type = type;
+    }
+
+    public string GetTypeAttr(){
+        return _type;
+    }
+
+    public void SetPresentation(string presentation){
+        _presentation = presentation;
+    }
+
+    public string GetPresentation(){
+        return _presentation;
+    }
+
+    public void SetId(int id){
+        _id = id;
+    }
+
+    public int GetId(){
+        return _id;
+    }
+
+
+
     public string StringRepresentation(){
         return "a";
     }
