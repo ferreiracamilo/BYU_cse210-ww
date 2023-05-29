32c9d23506df28ba10581b3351fea29a7b256bf5
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-28 22:53:50 -0300

    update getters and setters

--- final/FinalProject/VisitControl.cs
+++ final/FinalProject/VisitControl.cs
@@ -1,6 +1,14 @@
 public class VisitControl : Visit, IDataSL{
     private List<string> _valuations;
 
+    public void SetValuations(List<string> valuations){
+        _valuations = valuations;
+    }
+
+    public List<string> GetValuations(){
+        return _valuations;
+    }
+
     public void AddValuation(string valuation){
         _valuations.Add(valuation);
     }
--- final/FinalProject/VisitDiscomfort.cs
+++ final/FinalProject/VisitDiscomfort.cs
@@ -1,6 +1,14 @@
 public class VisitDiscomfort : Visit, IDataSL{
     private List<string> _synthoms;
 
+    public void SetSynthoms(List<string> synthoms){
+        _synthoms = synthoms;
+    }
+
+    public List<string> GetSynthoms(){
+        return _synthoms;
+    }
+
     public void AddSynthom(string synthom){
         _synthoms.Add(synthom);
     }
