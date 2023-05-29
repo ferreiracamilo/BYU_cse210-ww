4bf6a10febc5ac29bbf82982ad2ff5039cd4107c
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-28 22:39:43 -0300

    setter and getter

--- final/FinalProject/SimpleTreatment.cs
+++ final/FinalProject/SimpleTreatment.cs
@@ -1,6 +1,14 @@
 public class SimpleTreatment : Treatment, IDataSL{
     private List<Drug> _drugs;
 
+    public void SetDrugs(List<Drug> drugs){
+        _drugs = drugs;
+    }
+
+    public List<Drug> GetDrugs(){
+        return _drugs;
+    }
+
     public void AddDrug(Drug drug){
         _drugs.Add(drug);
     }
