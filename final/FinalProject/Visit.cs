7d373578bb67252cd0e760fa14b9281a594119ef
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-05-28 22:52:04 -0300

    setters and getters

--- final/FinalProject/Visit.cs
+++ final/FinalProject/Visit.cs
@@ -2,4 +2,28 @@ public class Visit{
     protected Pet _patient;
     protected DateOnly _date;
     protected int _id;
+
+    public virtual void SetPatient(Pet patient){
+        _patient = patient;
+    }
+
+    public virtual Pet GetPatient(){
+        return _patient;
+    }
+
+    public virtual void SetDate(DateOnly date){
+        _date = date;
+    }
+
+    public virtual DateOnly GetDate(){
+        return _date;
+    }
+
+    public virtual void SetId(int id){
+        _id = id;
+    }
+
+    public virtual int GetId(){
+        return _id;
+    }
 }
